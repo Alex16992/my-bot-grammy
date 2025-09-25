d13501de445ecaa2e9f8c3be69c20c5ec289c174
Alex_1699 <lehad600@gmail.com>
2025-09-25 13:17:18 +0500

    Убраны лишние комменты

--- src/menu/cryptoMenu.ts
+++ src/menu/cryptoMenu.ts
@@ -1,7 +1,5 @@
 import { InlineKeyboard } from "grammy";
 
-// Создайте простое меню.
-
 export const cryptoMenu = new InlineKeyboard()
   .text("Bitcoin", "bitcoin")
   .row()
@@ -10,20 +8,3 @@ export const cryptoMenu = new InlineKeyboard()
   .text("Litecoin", "litecoin")
   .row()
   .text("Назад", "back");
-
-// .text("Litecoin", async (ctx) => {
-//   try {
-//     // Запрос к CoinGecko API
-//     const res = await fetch(
-//       "https://api.coingecko.com/api/v3/simple/price?ids=litecoin&vs_currencies=usd"
-//     );
-//     const data = await res.json();
-
-//     const ltcPrice = data.litecoin.usd;
-
-//     // Отправляем ответ пользователю
-//     await ctx.reply(`💰 Курс лайткоина:\nLitecoin: $${ltcPrice}`);
-//   } catch (err) {
-//     console.error(err);
-//     await ctx.reply("Ошибка при получении курса криптовалют.");
-//   }
